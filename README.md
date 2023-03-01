86a4c71fbcf3a95e29c5b049a653cf7965580b24
AntonScheving <115781123+AntonScheving@users.noreply.github.com>
2023-03-01 16:34:28 +0000

    Added video to README

--- README.md
+++ README.md
@@ -17,15 +17,17 @@
 - [License](#license)
 
 
-<br>
+
 
 ![GitHub repo size](https://img.shields.io/github/repo-size/AntonScheving/README-Generator?style=plastic)
 
   ![GitHub top language](https://img.shields.io/github/languages/top/AntonScheving/README-Generator?style=plastic)
 
-<br>
+
 
 ## Description
+Click here to watch the 
+<a href="https://youtu.be/9iVQFaRaNV8"><strong>README Generator video demo</a></strong>
 
   The README Generator project is a command-line tool that automatically generates a README.md file for a project based on user input. The tool is written in JavaScript and prompts the user with questions about the project, such as the project title, description, installation instructions, usage instructions, contribution guidelines, and more.
 
@@ -34,13 +36,23 @@ The motivation behind creating this project was to provide a solution to the pro
 The project solves the problem of creating a professional-looking and well-documented README.md file for a project. The tool ensures that all the necessary sections are included and formatted correctly, and it almost eliminates the need for manual formatting and editing.
 
 Through this project, I learned how to use Node.js to build a command-line application, how to prompt users with questions and store their responses, and how to dynamically generate a Markdown file. I also gained experience in working with Git and GitHub for version control.
-<br><br><br>
+
+
+
+
+
+
+
+
 
 <p align="center">
-  <img alt="README Generator Code screenshot" [Screenshot] src="assets/images/README-Generator-Screenshot.png"><br>
-README Generator code 
+  <img alt="README Generator screenshot" [Screenshot] src="assets/images/README-Generator-Screenshot.png"><br>
+README Generator code screenshot
 </p>
-<br>
+
+
+
+
 
 ## Installation
 
@@ -68,14 +80,21 @@ npm install --save inquirer fs
 node index.js
 ```
 
-1. Answer the prompts in the terminal: The generator will prompt you with a series of questions about your project. Answer each question as accurately as possible. See the Usage section to learn how to answer long-form questions.
-2. Generate README.md file: Once you have answered all the questions, the generator will generate the README.md file based on your answers. The generated file will be saved in the project directory.
-3. Review and edit the generated README.md file: Review the generated README.mdfile and make any necessary edits or additions to ensure that it accurately represents your project and provides all the necessary information.
+1. Answer the prompts in the terminal: The generator will prompt you with a series of questions about your project. Answer each question as accurately as possible. See the Usage section to learn how to answer longform questions.
+2. Generate README.md file: Once you have answered all the questions, the generator will generate the **`README.md`** file based on your answers. The generated file will be saved in the project directory.
+3. Review and edit the generated **`README.md`** file: Review the generated **`README.md`** file and make any necessary edits or additions to ensure that it accurately represents your project and provides all the necessary information.
 
-That's it! With these steps, you can generate a professional-looking README.md file for your project with minimal effort.
+That's it! With these steps, you can generate a professional-looking **`README.md`** file for your project with minimal effort.
 
-README Generator is built with the following tools and libraries: <ul><li>JavaScript</li> <li>Node.js</li> <li>NPM packages - inquirer & fs</li> <li>Markdown</li></ul>
-<br>
+
+
+
+
+README Generator is built with the following tools and libraries: <ul><li>First item</li> <li>Second item</li> <li>Third item</li></ul>
+
+
+
+
 
 ## Usage
  
@@ -92,10 +111,13 @@ Here are step-by-step instructions for using the README Generator:
 - Press colon ‘:wq’ and enter to save and exit the editor to get to the next question
 1. The generator will store your responses in an object.
 2. Once you've answered all the questions, the generator will use that object to dynamically generate the README.md file.
-3. The generated README.md file will be located in the same directory as the generator files. Each generated file will override the previous file generated.
-4. Copy your generated README.md file and paste it into your own project folder.
+3. The generated README.md file will be located in the same directory as the generator files. Each generated file will overide the previous file generated.
+4. Copy your generated [README.md](http://README.md) file and paste it into your own project folder.
 
-<br>
+
+
+
+
 
 ## Contribution
  
@@ -114,12 +136,20 @@ We welcome contributions in the form of bug reports, feature requests, code cont
 ### **Code of Conduct**
 
 We expect all contributors to adhere to the **[Contributor Covenant](https://www.contributor-covenant.org/)**. Please be respectful and inclusive in your contributions and interactions with others.
-<br><br>
+
+
+
+
+
 
 ## Tests
  
 After the README Generator has generated the README.md file based on your input, it is important to test the file to ensure that it contains all the necessary sections and information, and that the formatting is correct. 
-<br><br>
+
+
+
+
+
 
 ## GitHub
 
@@ -128,13 +158,19 @@ After the README Generator has generated the README.md file based on your input,
 
 
 <p>Visit my website: <strong><a href="www.antonscheving.com">AntonScheving.com</a></strong></p>
-<br>
+
+
+
+
 
 ## Contact
 
 Feel free to reach out to me on my email:
 anton@scheving.email
-<br><br>
+
+
+
+
 
 ## License
 
--- index.js
+++ index.js
@@ -1,133 +1,121 @@
-console.log("\n\n This is a README.md file generator. Please read the included README file before using this application. \n\n Please type your answers to the questions below to generate your own README.md file.\n To type your answers press \n- 'Enter' \n- Then press 'i' \n- Press Ctrl v (Windows) - command v (Mac OS) to paste your text or start typing directly into the editor\n- Press 'escape' \n- Press colon ':' \n- Press 'wq' and 'Enter'\n\n");
+console.log(
+  "\n\n This is a README.md file generator. Please read the included README file before using this application. \n\n Please type your answers to the questions below to generate your own README.md file.\n To type your answers press \n- 'Enter' \n- Then press 'i' \n- Press Ctrl v (Windows) - command v (Mac OS) to paste your text or start typing directly into the editor\n- Press 'escape' \n- Press colon ':' \n- Press 'wq' and 'Enter'\n\n"
+);
 
 const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
-inquirer.prompt(
-// array of questions for user
-// const questions = 
-[
-    {
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your project?\n',
-  },
-  {
-    type: 'input',
-    name: 'repoTitle',
-    message: 'What is the exact title of your repository?\n',
-  },
-  {
-    type: 'editor',
-    name: 'description',
-    message: '\nPlease enter a description of your project: \nExplain the what, why, and how of your project. \nUse the following questions as a guide: \n\t- What was your motivation? \n\t- Why did you build this project? \n\t- What problem does it solve? \n\t- What did you learn?\n'
-  },
-  {
-    type: 'input',
-    name: 'deployedWebsite',
-    message: 'Please enter deployed website URL:\n',
-  },
-  {
-    type: 'input',
-    name: 'imgAlt',
-    message: 'Please enter your screenshot Alt:\n',
-  },
-  {
-    type: 'input',
-    name: 'screenshot',
-    message: 'Please enter relative path to screenshot:\n',
-  },
-  {
-    type: 'input',
-    name: 'screenshotSubtitle',
-    message: 'Please enter screenshot subtitle:\n',
-  },
-  {
-    type: 'editor',
-    name: 'installation',
-    message: 'Please enter installation instructions for your project:\n',
-  },
-  {
-    type: 'input',
-    name: 'librariesAndTools',
-    message: 'Please enter libraries and tools used for your project. Write them in a list like this: \n<li>First item</li> <li>Second item</li> <li>Third item</li>:\n',
-  },
-  {
-    type: 'editor',
-    name: 'usage',
-    message: 'Please enter usage information for your project:\n',
-  },
-  {
-    type: 'list',
-    name: 'license',
-    message: '\n Please choose a license for your project\n (Visit https://choosealicense.com/ if you are unsure, which license to choose):',
-    choices: ['MIT', 'GPL-3-0', 'apache-2-0', 'BSD-3-clause', 'ISC', '0bsd'],
-  },
-  
-  {
-    type: 'editor',
-    name: 'contribution',
-    message: '\nPlease enter contribution guidelines for your project:\n',
-  },
-  {
-    type: 'editor',
-    name: 'tests',
-    message: 'Please enter test instructions for your project:\n',
-  },
-  {
-    type: 'input',
-    name: 'github',
-    message: 'What is your GitHub username?\n',
-  },
-  {
-    type: 'input',
-    name: 'website',
-    message: 'What is your website URL?\n',
-  },
-  {
-    type: 'input',
-    name: 'webLinkPlaceholder',
-    message: 'What is the placeholder for the website URL?\n',
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: 'What is your email address?\n',
-  },
-]).then((answers)=> {
-  console.log(answers)
+inquirer
+  .prompt(
+    // array of questions for user
 
-// fs.writeFile(`index.html`, HTMLTemplate, (error) => error ? console.error(error) : console.log(`Success`)
+    [
+      {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?\n",
+      },
+      {
+        type: "input",
+        name: "repoTitle",
+        message: "What is the exact title of your repository?\n",
+      },
+      {
+        type: "editor",
+        name: "description",
+        message:
+          "\nPlease enter a description of your project: \nExplain the what, why, and how of your project. \nUse the following questions as a guide: \n\t- What was your motivation? \n\t- Why did you build this project? \n\t- What problem does it solve? \n\t- What did you learn?\n",
+      },
+      {
+        type: "input",
+        name: "deployedWebsite",
+        message: "Please enter deployed website URL:\n",
+      },
+      {
+        type: "input",
+        name: "imgAlt",
+        message: "Please enter your screenshot Alt:\n",
+      },
+      {
+        type: "input",
+        name: "screenshot",
+        message: "Please enter relative path to screenshot:\n",
+      },
+      {
+        type: "input",
+        name: "screenshotSubtitle",
+        message: "Please enter screenshot subtitle:\n",
+      },
+      {
+        type: "editor",
+        name: "installation",
+        message: "Please enter installation instructions for your project:\n",
+      },
+      {
+        type: "input",
+        name: "librariesAndTools",
+        message:
+          "Please enter libraries and tools used for your project. Write them in a list like this: \n<li>First item</li> <li>Second item</li> <li>Third item</li>:\n",
+      },
+      {
+        type: "editor",
+        name: "usage",
+        message: "Please enter usage information for your project:\n",
+      },
+      {
+        type: "list",
+        name: "license",
+        message:
+          "\n Please choose a license for your project\n (Visit https://choosealicense.com/ if you are unsure, which license to choose):",
+        choices: [
+          "MIT",
+          "GPL-3-0",
+          "apache-2-0",
+          "BSD-3-clause",
+          "ISC",
+          "0bsd",
+        ],
+      },
 
-// fs.writeFile(`${answers.title}.html`, HTMLTemplate, (error) =>
-//       error ? console.error(error) : console.log(`Success!`)
+      {
+        type: "editor",
+        name: "contribution",
+        message: "\nPlease enter contribution guidelines for your project:\n",
+      },
+      {
+        type: "editor",
+        name: "tests",
+        message: "Please enter test instructions for your project:\n",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?\n",
+      },
+      {
+        type: "input",
+        name: "website",
+        message: "What is your website URL?\n",
+      },
+      {
+        type: "input",
+        name: "webLinkPlaceholder",
+        message: "What is the placeholder for the website URL?\n",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?\n",
+      },
+    ]
+  )
+  .then((answers) => {
+    console.log(answers);
 
-      // fs.writeFile(`README.md`, `${answers.title}`, (error) =>
-      // error ? console.error(error) : console.log(`Success!`)
-            fs.writeFile(`README.md`, generateMarkdown(answers), (error) =>
-      error ? console.error(error) : console.log(`Success! You have now created a README.md `)
-);
-});
-
-
-
-// console.log(questions);
-// console.log(inquirer);
-
-
-// function to write README file
-// function writeToFile(fileName, data) {
-// }
-
-// function to initialize program
-// const init() => {
-
-// }
-// function init() {
-
-// }
-
-// function call to initialize program
-// init();
-// inquirer.createPromptModule(questions)
+    fs.writeFile(`README.md`, generateMarkdown(answers), (error) =>
+      error
+        ? console.error(error)
+        : console.log(`Success! You have now created a README.md `)
+    );
+  });
